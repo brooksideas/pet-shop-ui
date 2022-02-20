ae5d741435a624d35f78cc61a8890fb6bf061f03
Brook Tarekegn <brooksideas@gmail.com>
2022-02-21 00:34:26 +0300

    Structure: Added Vuetify Plugin to the project + Adjusted Roboto as default font

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
-  <div id="app">    
-   <router-view/> 
-  </div>
+  <v-app id="app">
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<script> 
-
+<script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
-
-<style>
-#app {
-  font-family: Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+ 
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
+Vue.use(Vuetify);
+ 
+export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: "#4EC690", 
+            secondary: "#DDE3E2",
+            background: "#F7F9FC",
+            white: "#FFFFFF", 
+            black: "#000000",
+            grey: "#69817B",
+            blue: "#2196F3",
+            yellow: "#F09E00",
+            green: "#008655"
+          }
+        }
+    }
+});
