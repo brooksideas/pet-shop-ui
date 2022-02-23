ebf3927d9cf7bc26a1b67f93fe24bff0d47cac7d
Brook Tarekegn <brooksideas@gmail.com>
2022-02-23 11:55:22 +0300

    Feat: Product Card Implementation + BreadCrumb Implemetation

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,41 +1,28 @@
 <template>
-  <v-card class="mx-auto" max-width="344">
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-      height="200px"
-    ></v-img>
+  <v-card class="productContainer">
+    <img src="../assets/Product_Image.svg" alt="Product" height="200px" />
 
-    <v-card-title> Top western road trips </v-card-title>
+    <v-card-title class="productTitle text-decoration-underline">
+      Top western road trips
+    </v-card-title>
 
-    <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+    <v-card-subtitle class="">
+      <label class="productSubTitle"> Animonda </label>
+      <label class="black--text productAmountLabel"> 200kn </label>
+    </v-card-subtitle>
 
     <v-card-actions>
-      <v-btn color="orange lighten-2" text> Explore </v-btn>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon @click="show = !show">
-        <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+      <v-btn color="primary">
+        <v-icon left class="cartIcon"> mdi-cart </v-icon>
+        <label class="white--text productAddLabel"> add to cart </label>
       </v-btn>
     </v-card-actions>
-
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
-
-        <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he
-          could deliver. You won't have time for sleeping, soldier, not with all
-          the bed making you'll be doing. Then we'll go with that data file!
-          Hey, you add a one and two zeros to that or we walk! You're going to
-          do his laundry? I've got to find a way to escape.
-        </v-card-text>
-      </div>
-    </v-expand-transition>
   </v-card>
 </template>
 
 <script>
+import "../styles/product.scss";
+
 export default {
   name: "ProductCard",
   data: () => ({
